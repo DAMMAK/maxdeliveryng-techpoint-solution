902f62848fc0126f32d899bac65ab4e534edef76
DAMMAK <adekoyafelix@gmail.com>
2019-03-22 10:10:52 +0100

    Fix All test suites

--- src/morse.js
+++ src/morse.js
@@ -65,12 +65,11 @@ Object.freeze(MORSE_CODE);
  */
 function decodeMorse(morseCode) {
   let transformText = Array();
-
   //code = code.replace(/ {1,}/g, " ");
-
-  const codeArr = morseCode.split(" "); //convert the string into an array
+  let codeArr = rmSpaces(morseCode);
+  // const codeArr = mCode.split(" "); //convert the string into an array
   // console.log("[COde Array]=>", codeArr);
-  codeArr.forEach((element, index) => {
+  for (let index = 0; index < codeArr.length; index++) {
     var morse = codeArr[index]; //Get Each Item from the arry using the index
 
     if (morse === "" && codeArr[index + 1] === "") {
@@ -79,10 +78,34 @@ function decodeMorse(morseCode) {
     } else {
       transformText.push(MORSE_CODE[morse]); //use the item as an object key to get the value form the object of MORSE_CODE
     }
-  });
+  }
 
   //console.log("[Morse Character]:", transformText.join(""));
   return transformText.join(""); //Join The Array to make a single string
 }
+
+var rmSpaces = codeArr => {
+  let rData = codeArr.split(" ");
+  let keys = Object.keys(MORSE_CODE);
+  for (let index = 0; index < codeArr.length; index++) {
+    let cur = rData[0];
+    if (!keys.includes(cur)) {
+      rData.splice(0, 1);
+    } else {
+      break;
+    }
+  }
+
+  for (let index = rData.length; index > 0; index--) {
+    let curIndex = rData.length - 1;
+    let cur = rData[curIndex];
+    if (!keys.includes(cur)) {
+      rData.splice(rData.length - 1, 1);
+    } else {
+      break;
+    }
+  }
+  return rData;
+};
 
 module.exports = decodeMorse;
